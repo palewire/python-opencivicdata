57433a3b045cba4c37a3afa4953951afe4c14e70
palewire <ben.welsh@gmail.com>
2017-12-20 18:38:54 -0800

    Moved transaction models into a separate file for #

--- opencivicdata/campaign_finance/models/__init__.py
+++ opencivicdata/campaign_finance/models/__init__.py
@@ -1,10 +1,13 @@
 # flake8: NOQA
 from .committees import (
     Committee, CommitteeCandidacyDesignation, CommitteeIdentifier,
-    CommitteeName, CommitteeSource, CommitteeStatus, CommitteeType,
+    CommitteeName, CommitteeSource, CommitteeStatus, CommitteeType
 )
 from .filings import (
     Filing, FilingAction, FilingActionSummaryAmount, FilingIdentifier,
-    FilingSource, Transaction, TransactionIdentifier, TransactionNote,
-    TransactionSource,
+    FilingSource
+)
+from .transactions import (
+    Transaction, TransactionIdentifier, TransactionNote,
+    TransactionSource
 )
