2d6f43e89c03f487e2d8cf2bcd01b40d949becde
palewire <b@palewi.re>
2022-09-09 18:37:49 -0700

    0.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ extras_require = {
 
 setup(
     name="ccdc-opencivicdata",
-    version="0.0.1",
+    version="0.0.3",
     author="Ben Welsh",
     author_email="b@palewi.re",
     license="BSD",
